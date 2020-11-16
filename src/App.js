54b6d9fc78c53402b023a59ff0da3a95e9da0229
Allisyn Abrams <allisynabrams@Allisyns-MBP-2.fios-router.home>
2020-11-16 13:23:10 -0500

    request to songs index and request to songs destroy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
+import React, {useState, useEffect} from 'react'
 import { Route, Link, Switch } from 'react-router-dom';
 import Playlist from './components/Playlist/Playlist'
 import Favorites from './components/Favorites/Favorites';
 import Form from './components/Form/Form';
 
 function App() {
+
+  const url = 'https://tunr-app-aa-api.herokuapp.com/songs'
+
+  const [songs, setSongs] = useState([])
+  const [faves, setFaves] = useState([])
+
+  const emptySong = {
+		title: '',
+		artist: '',
+		time: '',
+  }
+  
+  const getSongs = async () => {
+    try {
+      const res = await fetch(url)
+      const songsData = await res.json()
+      setSongs(songsData)
+    } catch (err) {
+      console.log('this is err - ', err)
+    }
+  };
+  
+  console.log('this is songs - ', songs)
+
+	useEffect(() => {
+		getSongs();
+  }, []);
+  
+  const deleteSong = (song) => {
+		fetch(url + '/' + song.id, {
+			method: 'delete',
+		}).then((response) => getSongs());
+	};
+
+
+
   return (
 		<div className='App'>
 			<header className='header'>
@@ -18,7 +55,7 @@ function App() {
 					path='/'
 					render={(routerProps) => (
 						<div>
-							<Playlist {...routerProps} />
+							<Playlist {...routerProps} songs={songs} deleteSong={deleteSong}/>
 							<Favorites {...routerProps} />
 							<Form {...routerProps} />
 						</div>
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,10 +1,31 @@
 import React, {useState, useEffect} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Playlist = (props) => {
+    const {songs} = props
 
-    return (
-        <p>Playlist</p>
-    )
+    const loaded = () => (
+		<div>
+		{songs.map((song) => (
+			<article>
+			<p>{song.title}</p>
+			<p>{song.artist}</p>
+			<p>{song.time}</p>
+			<button
+				onClick={() => {
+				props.deleteSong(song);
+				}}>X</button>
+			<FontAwesomeIcon
+			    icon={faHeart}
+				// onClick={() => props.addToFaves(song)}
+			/>
+			</article>
+				))}
+			</div>
+		);
+
+    return songs.length > 0 ? loaded() : <h1>Loading...</h1>
 }
 
 export default Playlist
